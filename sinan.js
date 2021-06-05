3fbeb5896ff6b9a5ba933ef2f5df4c4a0ccc18ef
Sinan Demir <sinan.28042001@gmail.com>
2021-06-05 23:44:48 +0300

    orta ve zor mod eklendi

--- sinan.js
+++ sinan.js
@@ -1,9 +1,17 @@
-//  ------------------ DOM elemanları ------------------ //
+//  ------------------------------- DOM elements ------------------------------- //
 
 const btn = document.querySelector('.btn');
 const choose1 = document.querySelector('.choose-1');
 const choose2 = document.querySelector('.choose-2');
 const choose3 = document.querySelector('.choose-3');
+const choose4 = document.querySelector('.choose-4');
+const choose5 = document.querySelector('.choose-5');
+const choose6 = document.querySelector('.choose-6');
+
+const easyMode = document.querySelector('.easy');
+const mediumMode = document.querySelector('.medium');
+const hardMode = document.querySelector('.hard');
+const circleMode = document.querySelector('.circle');
 
 const lifeText = document.querySelector('.life-text');
 const lifeScore = document.querySelector('.life-score');
@@ -11,17 +19,29 @@ const lifeScore = document.querySelector('.life-score');
 const resultText = document.querySelector('.result-text');
 const startText = document.querySelector('.start-text');
 
-//  ------------------ DOM elemanları ------------------ //
+//  ------------------------------- DOM elements ------------------------------- //
 
-//  ------------------ Başlangıç Değerleri ------------------ //
+//  ------------------------------- Initial Values  ------------------------------- //
 
 let backgroudColor;
-let life = 5;
+let life;
 let score = 0;
 let wrongScore = 0;
-//  ------------------ Başlangıç Değerleri ------------------ //
+let gameMode = 0; // 0-> easy, 1-> medium, 2-> hard
+switch (gameMode) {
+    case 0:
+        life = 5;
+        break;
+    case 1:
+        life = 4;
+        break;
+    case 2:
+        life = 2;
+        break;
+}
+//  ------------------------------- Initial Values  ------------------------------- //
 
-//  ------------------ Fonksiyonlar ------------------ //
+//  ------------------------------- Functions ------------------------------- //
 function visibleEl(...args) {
     for (let arg of args) {
         arg.classList.remove('hidden');
@@ -41,7 +61,7 @@ function changeValue(...args) {
 }
 
 function randomHex() {
-    const hex = [0, 1, 'A', 2, 3, 'B', 4, 5, 6, 'C', 7, 'D', 8, 'E', 'F', 9];
+    const hex = [0, 1, 'A', 2, 3, 'B', 4, 5, 'C', 6, 7, 'D', 8, 'E', 'F', 9];
     let hexCode = '#';
     let randomNumber;
     for (let i = 0; i < 6; i++) {
@@ -51,15 +71,31 @@ function randomHex() {
     return hexCode;
 }
 
+function createRandomNumber(number) {
+    return Math.floor(Math.random() * number);
+}
+
 function ready() {
-    // Doğru hex kutusunu belirleyip arkaplan hexini ona verelim.
-    let randomNumber = Math.floor(Math.random() * 3);
-    const boxes = [choose1, choose2, choose3];
+    let boxes = [choose1, choose2, choose3];
+    let randomNumber;
+
+    // Oyun moduna göre boxes arrayini ayarlayalım ve doğru olacak kutuyu seçelim.
+    if (gameMode === 0) {
+        randomNumber = createRandomNumber(3);
+    } else if (gameMode === 1) {
+        randomNumber = createRandomNumber(4);
+        boxes.push(choose4);
+    } else if (gameMode === 2) {
+        randomNumber = createRandomNumber(6);
+        boxes.push(choose4, choose5, choose6);
+    }
+
+    // Doğru hex kutusunu belirledik. Arkaplan hexini ona verelim.
     backgroudColor = randomHex();
     document.body.style.backgroundColor = backgroudColor;
-
-    // Yanlış hex kutularına randomHex verelim.
     boxes[randomNumber].textContent = backgroudColor;
+
+    // Yanlış hex kutularına random hex verelim.
     boxes.splice(randomNumber, 1);
     for (let i = 0; i < boxes.length; i++) {
         boxes[i].textContent = randomHex();
@@ -68,7 +104,7 @@ function ready() {
 
 function check(arg) {
     if (arg.textContent !== backgroudColor) {
-        console.log('yanlış');
+        // console.log('yanlış');
         hiddenEl(arg);
         life--;
         resultText.style.color = 'red';
@@ -76,10 +112,20 @@ function check(arg) {
         visibleEl(resultText);
         wrongScore++;
     } else {
-        console.log('doğru');
+        // console.log('doğru');
         score++;
         ready();
-        visibleEl(choose1, choose2, choose3);
+        switch (gameMode) {
+            case 0:
+                visibleEl(choose1, choose2, choose3);
+                break;
+            case 1:
+                visibleEl(choose1, choose2, choose3, choose4);
+                break;
+            case 2:
+                visibleEl(choose1, choose2, choose3, choose4, choose5, choose6);
+                break;
+        }
         resultText.style.color = 'green';
         changeValue([resultText, 'Correct :)']);
         visibleEl(resultText);
@@ -87,29 +133,90 @@ function check(arg) {
     }
 
     // 2 kez yanlış yaparsa tek kutuya düşmemesi için..
-    if (wrongScore === 2) {
+    if (wrongScore === (gameMode === 0 ? 2 : 3)) {
         wrongScore = 0;
         ready();
-        visibleEl(choose1, choose2, choose3);
+        switch (gameMode) {
+            case 0:
+                visibleEl(choose1, choose2, choose3);
+                break;
+            case 1:
+                visibleEl(choose1, choose2, choose3, choose4);
+                break;
+            case 2:
+                visibleEl(choose1, choose2, choose3, choose4, choose5, choose6);
+                break;
+        }
     }
 
     // Canı kalmadıysa..
     if (life === 0) {
-        hiddenEl(choose1, choose2, choose3, resultText);
+        switch (gameMode) {
+            case 0:
+                hiddenEl(choose1, choose2, choose3, resultText);
+                break;
+            case 1:
+                hiddenEl(choose1, choose2, choose3, choose4, resultText);
+                break;
+            case 2:
+                hiddenEl(
+                    choose1,
+                    choose2,
+                    choose3,
+                    choose4,
+                    choose5,
+                    choose6,
+                    resultText
+                );
+                break;
+        }
         visibleEl(btn);
         changeValue(
             [startText, 'Game Over!'], [lifeScore, score], [lifeText, 'Score']
         );
-        life = 5;
+        switch (gameMode) {
+            case 0:
+                life = 5;
+                break;
+            case 1:
+                life = 4;
+                break;
+            case 2:
+                life = 2;
+                break;
+        }
         score = 0;
         wrongScore = 0;
     }
 }
-//  ------------------ Fonksiyonlar ------------------ //
 
-//  ------------------ Event Listeners ------------------ //
+function modeEvents(mode, px) {
+    gameMode = mode;
+    circleMode.style.margin = `0px ${px}px`;
+}
+//  ------------------------------- Functions ------------------------------- //
+
+//  ------------------------------- Event Listeners ------------------------------- //
 btn.addEventListener('click', function() {
-    visibleEl(choose1, choose2, choose3);
+    switch (gameMode) {
+        case 0:
+            visibleEl(choose1, choose2, choose3);
+            hiddenEl(choose4, choose5, choose6);
+            break;
+        case 1:
+            visibleEl(choose1, choose2, choose3, choose4);
+            hiddenEl(choose5, choose6);
+            life = 4;
+            changeValue([lifeText, life]);
+
+            break;
+        case 2:
+            visibleEl(choose1, choose2, choose3, choose4, choose5, choose6);
+            life = 2;
+            changeValue([lifeText, life]);
+
+            break;
+    }
     hiddenEl(btn);
     changeValue([startText, 'Choose one'], [lifeText, 'Life'], [lifeScore, life]);
     ready();
@@ -123,4 +230,22 @@ choose2.addEventListener('click', () => {
 choose3.addEventListener('click', () => {
     check(choose3);
 });
-//  ------------------ Event Listeners ------------------ //
+choose4.addEventListener('click', () => {
+    check(choose4);
+});
+choose5.addEventListener('click', () => {
+    check(choose5);
+});
+choose6.addEventListener('click', () => {
+    check(choose6);
+});
+easyMode.addEventListener('click', () => {
+    modeEvents(0, 18);
+});
+mediumMode.addEventListener('click', () => {
+    modeEvents(1, 80);
+});
+hardMode.addEventListener('click', () => {
+    modeEvents(2, 147);
+});
+//  ------------------------------- Event Listeners ------------------------------- //
